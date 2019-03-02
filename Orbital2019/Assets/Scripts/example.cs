5ecb2fb1fc122d047c7dafc8a0e508eab030b5fe
Whenti <l.quentin@sfr.fr>
2019-03-02 15:35:09 +0100

    init with unity

---
+++ Orbital2019/Assets/Scripts/example.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class example : MonoBehaviour
+{
+
+    [SerializeField]
+    int index;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
